4d182fcf48e1df7fab773883e9180dac24017609
Cong <licong2020@qq.com>
2023-03-01 16:19:07 +0800

    2023.3.1第2次提交

--- src/main/java/com/CONG/git/GItTest.java
+++ src/main/java/com/CONG/git/GItTest.java
@@ -3,5 +3,6 @@ package com.CONG.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world 2!");
     }
 }
